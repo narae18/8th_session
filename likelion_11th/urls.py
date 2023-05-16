e3e0e3d7d4e7ce5204a9e4d633e90aa98e9a53bf
narae18 <starestarstar@naver.com>
2023-05-16 18:22:43 +0900

    last

--- likelion_11th/urls.py
+++ likelion_11th/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from main import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
+    path('accounts/', include('accounts.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
